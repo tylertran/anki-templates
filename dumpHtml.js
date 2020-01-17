fcfb11dbc99861c75a8770cdd026828ccb9d8a3d
Tyler Tran <tvtran2@gmail.com>
2020-01-17 17:49:57 -0600

    Make dumping HTML always recalculate the DOM
    This avoids issues where the DOM is captured before other scripts have finished

--- dumpHtml.js
+++ dumpHtml.js
@@ -7,20 +7,16 @@ SHOW_HTML_BUTTON.innerHTML = "Show HTML";
 
 document.body.appendChild(SHOW_HTML_BUTTON);
 
-// This value for some reason is being captured as some value < 1, which
-// screws up restoring the page after hiding the HTML view
-document.getElementById('qa').style.setProperty('opacity', '1');
-
 const ROOT_NODE = document.documentElement;
-const BACKUP_SRC = ROOT_NODE.innerHTML;
-
-// Adapated from https://stackoverflow.com/a/1815227
-const DISPLAY_SRC = '<pre>' + BACKUP_SRC.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</pre>';
+let backup_src = '';
 
 function showHTML() {
-    document.body.innerHTML = HIDE_HTML_BUTTON + DISPLAY_SRC;
+	// Adapated from https://stackoverflow.com/a/1815227
+	backup_src = ROOT_NODE.innerHTML;
+	const display_src = '<pre>' + backup_src.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</pre>';
+    document.body.innerHTML = HIDE_HTML_BUTTON + display_src;
 }
 
 function hideHTML() {
-    ROOT_NODE.innerHTML = BACKUP_SRC;
+    ROOT_NODE.innerHTML = backup_src;
 }
